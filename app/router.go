5db6a708dd5884c0187b6034c282235908ab007b
frchandra <nismara.chandra@gmail.com>
2023-04-17 15:29:44 +0700

    reconfiguring routes

--- app/router.go
+++ app/router.go
@@ -31,53 +31,59 @@ func NewRouter(
 		router = gin.Default()
 	}
 
-	//Health Check
-	router.GET("/", homeController.HealthCheck)
-	router.GET("/api/v1", homeController.HealthCheck)
+	apiv1 := "/api/v1"
 
-	//Midtrans Webhook
-	webhook := router.Group("api/v1")
+	//Health Check (scope: public)
+	router.GET("/", homeController.HealthCheck)
+	router.GET(apiv1, homeController.HealthCheck)
+
+	//Midtrans Webhook (scope: public)
+	webhook := router.Group(apiv1)
 	webhook.POST("/snap/payment/callback", snapController.HandleCallback)
 
-	//Public User Standard Auth Routes
-	public := router.Group("/api/v1")
-	public.POST("/user/refresh", authController.RefreshToken)
-	public.POST("/user/register_email", authController.RegisterByEmail)
-	public.POST("/user/otp", authController.VerifyOtp)
-	public.POST("/user/login", authController.Login)
+	//User Standard Auth Routes (scope: public)
+	auth := router.Group(apiv1 + "/user")
+	auth.POST("/refresh", authController.RefreshToken)
+	auth.POST("/register_email", authController.RegisterByEmail)
+	auth.POST("/otp", authController.VerifyOtp)
+	auth.POST("/login", authController.Login)
 
-	//Public Post Ticketing
-	public.Use(qrMiddleware.HandleScanQr).GET("/seat/:link", seatController.InfoByLink)
+	//Post Ticketing (scope: public)
+	qr := router.Group(apiv1).Use(qrMiddleware.HandleScanQr)
+	qr.GET("/seat/:link", seatController.InfoByLink)
 
-	//Public Pre Ticketing
-	public.Use(gateMiddleware.HandleAccess).GET("/seat_map", reservationController.GetSeatsInfo)
+	//Pre Ticketing (scope: public)
+	seatMap := router.Group(apiv1).Use(gateMiddleware.HandleAccess)
+	seatMap.GET("/seat_map", reservationController.GetSeatsInfo)
 
-	//Logged-In User Routes
-	user := router.Group("/api/v1").Use(userMiddleware.UserAccess)
-	user.POST("/user/logout", authController.Logout)
-	user.GET("/user", userController.CurrentUser)
-	user.GET("/user/tickets", userController.ShowMyTickets)
-	user.PATCH("/user", userController.UpdateInfo)
+	//User data (scope: buyer user)
+	user := router.Group(apiv1 + "/user").Use(userMiddleware.UserAccess)
+	user.POST("/logout", authController.Logout)
+	user.GET("/", userController.CurrentUser)
+	user.GET("/tickets", userController.ShowMyTickets)
+	user.PATCH("/", userController.UpdateInfo)
 
-	user.Use(gateMiddleware.HandleAccess).POST("/seat_map", reservationController.ReserveSeats) //Ticketing routes
-	user.Use(gateMiddleware.HandleAccess).GET("/checkout", txController.GetLatestTransactionDetails)
-	user.Use(gateMiddleware.HandleAccess).POST("/checkout", txController.InitiateTransaction)
+	//Ticketing routes (scope: buyer user)
+	userTicketing := router.Group(apiv1).Use(gateMiddleware.HandleAccess)
+	userTicketing.POST("/seat_map", reservationController.ReserveSeats)
+	userTicketing.GET("/checkout", txController.GetLatestTransactionDetails)
+	userTicketing.POST("/checkout", txController.InitiateTransaction)
 
-	//Admin Routes
-	admin := router.Group("/api/v1").Use(adminMiddleware.AdminAccess)
-	admin.GET("/admin/seat/:link", seatController.DetailsByLink)
-	admin.PUT("/admin/seat/:link", seatController.UpdateByLink)
-	admin.GET("/admin/seat/:link/:status", seatController.UpdateToStatus)
+	//Admin Routes (scope: admin user)
+	admin := router.Group(apiv1 + "/admin").Use(adminMiddleware.AdminAccess)
+	admin.GET("/seat/:link", seatController.DetailsByLink)
+	admin.PUT("/seat/:link", seatController.UpdateByLink)
+	admin.GET("/seat/:link/:status", seatController.UpdateToStatus)
 
-	admin.POST("/admin/open_the_gate", gateController.OpenGate)
-	admin.POST("/admin/close_the_gate", gateController.CloseGate)
+	admin.POST("/open_the_gate", gateController.OpenGate)
+	admin.POST("/close_the_gate", gateController.CloseGate)
 
-	admin.POST("/admin/set_to_production", gateController.SetToProduction)
-	admin.POST("/admin/set_to_sandbox", gateController.SetToSandbox)
+	admin.POST("/set_to_production", gateController.SetToProduction)
+	admin.POST("/set_to_sandbox", gateController.SetToSandbox)
 
-	admin.PATCH("/admin/qr_scan_behaviour", gateController.UpdateQrScanBehaviour)
-	admin.GET("/admin/get_app_config", gateController.GetAppConfig)
-	admin.GET("/admin/seats", seatController.AllDetails)
+	admin.PATCH("/qr_scan_behaviour", gateController.UpdateQrScanBehaviour)
+	admin.GET("/get_app_config", gateController.GetAppConfig)
+	admin.GET("/seats", seatController.AllDetails)
 
 	//public.POST("/user/register", authController.Register) //This route is no longer needed for current GMCO's ticketing case,
 	//public.POST("/user/sign_in", authController.SignIn) //but the code implementation in the controller is still remain in case of future use
